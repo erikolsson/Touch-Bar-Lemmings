4b5ddf5f60eea0fbb2d192d4516a50ae384fa71b
Erik Olsson <valross2@gmail.com>
2016-12-10 19:37:05 +0100

    Initial Commit

---
+++ TouchLemmings/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchLemmings
+//
+//  Created by Erik Olsson on 2016-12-10.
+//  Copyright © 2016 Erik Olsson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ TouchLemmings/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchLemmings
+//
+//  Created by Erik Olsson on 2016-12-10.
+//  Copyright © 2016 Erik Olsson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
